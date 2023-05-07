717204deca59e5c858df84036bd8dd95303925eb
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2023-05-07 13:57:02 +0500

    established routes for text and json repose

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,141 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
+import java.io.OutputStream; // used to write response body to network socket
+import java.io.InputStream; // used to read request body from network socket
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.URI;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    // static configuration for the server
+    static short PORT = 8000;
+    static String HOST = "" ;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    static Utilities utilities = new Utilities();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws Exception {
+        // fetching the host name
+        InetAddress address = InetAddress.getLocalHost();
+        HOST = address.getHostName();
+
+        // create an HTTP server
+        HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+
+        // routes for the API
+        server.createContext("/", new Home());
+        server.createContext("/parse", new ParseToString());
+        server.createContext("/parse/json", new ParseToJSON());
+//        server.createContext("/parse/xml", new ParseToXML());
+
+        // print the link to the server
+        System.out.println(String.format(">> Server Running On: http://%s:%d ", HOST, PORT));
+
+        // start the server
+        server.setExecutor(null);
+        server.start();
+    }
+
+    // Class for handling the Home/base route
+    static class Home implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            // response string
+            String response = "THIS IS HOME";
+
+            // sending response code
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+
+            // sending the actual response to the browser/client
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class ParseToString implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            // fetching request elements
+            String requestMethod = exchange.getRequestMethod();
+            Headers requestHeaders = exchange.getRequestHeaders();
+            URI requestURI = exchange.getRequestURI();
+            String query = requestURI.getQuery();
+            String path = requestURI.getPath();
+
+            // parsing the request body
+            InputStream requestBody = exchange.getRequestBody();
+            String responseBodyString = null;
+            try {
+                responseBodyString = utilities.ReqBodyToByteArray(requestBody);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            // declaring a string response
+            String response = "PARSED!\n";
+            response += String.format("Request method:          %s \n", requestMethod);
+            response += String.format("Request header(s):       %s \n", requestHeaders);
+            response += String.format("Query parameter(s):      %s \n", query);
+            response += String.format("Query path:              %s \n", path);
+            response += String.format("Response body string:    %s \n", responseBodyString);
+
+            // sending the response code
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            // sending the actual response
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    static class ParseToJSON implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            // fetching request elements
+            String requestMethod = exchange.getRequestMethod();
+            Headers requestHeaders = exchange.getRequestHeaders();
+            URI requestURI = exchange.getRequestURI();
+            String query = requestURI.getQuery();
+            String path = requestURI.getPath();
+
+
+            // parsing the request body
+            InputStream requestBody = exchange.GetRequestBody();
+            String responseBodyString = null;
+            try {
+                responseBodyString = utilities.ReqBodyToByteArray(requestBody);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            String jsonResponse = utilities.GetJSONResponse(
+                    requestMethod=requestMethod,
+                    requestHeaders=requestHeaders,
+                    responseBodyString=responseBodyString,
+                    query=query,
+                    path=path
+            );
+
+            // sending the response code
+            exchange.sendResponseHeaders(
+                    200,
+                    jsonResponse.getBytes().length
+            );
+            // sending the actual response
+            OutputStream os = exchange.getResponseBody();
+            os.write(jsonResponse.getBytes());
+            os.close();
         }
     }
 }
